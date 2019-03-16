6d8a6cabf004254e5137ef04b90a23835985a8f1
Vaibhav-Leo <vaibhav.shringarpure@jellyfish.co.uk>
2019-03-16 16:40:59 +0000

    Removed gulpfile as not being used anymore. Also added consolle message about the boilerplate being used

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ import ReactDOM from 'react-dom';
 import App from './app/App';
 
 const el = document.querySelector('#app');
+console.log('Using React Boilerplate created by Vaibhav Shringarpure');
 ReactDOM.render(<App />, el);
